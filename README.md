4c02f0efbbe0568e55c8487c4259fec6fd0447c8
Adrian Abreu <aabreuglez@gmail.com>
2016-03-23 17:08:38 +0000

    Initial version launched

---
+++ README.md
@@ -0,0 +1,42 @@
+# wordReferenceBot
+
+This is a telegram bot made on Node.js for helping with translations.
+
+I have trying to learn Englsh on my own. But I need to translate a lot of new words and with this bot, it's extremely easy :)
+
+Since wordreference's api is no longer available, I'm doing web scraping using [request](https://github.com/request/request) + [cheerio](https://github.com/cheeriojs/cheerio).
+
+##Commands
+
+```
+/spa - Translate the list of words from English to Spanish.
+/eng - Traduce esta lista de palabra de español a inglés.
+/help - Show help message.
+
+```
+
+##Features
+
+This bot uses a webhook for better response time, it also uses 
+
+##Run your own
+
+I have deployed it to Heroku with almost no problem.
+
+1. Get your own bot via [@BotFather](https://telegram.me/BotFather), make your own bot and get your token.
+2. Place your token in bot.js file.
+3. Create a heroku account and copy node's example.
+4. Replace those files with these. 
+5. Make a push to heroku master `git push heroku master`
+6. Enjoy
+
+## Working!
+
+![bot chat](botworks.png)
+
+## Documentation
+
+For more info of everything that has struggled me:
+
+- [Problems crawling wordreference](http://stackoverflow.com/questions/34860760/problems-crawling-wordreference)
+- [Configure a telegram bot webhook into an existing express app](http://mvalipour.github.io/node.js/2015/12/06/telegram-bot-webhook-existing-express/)
---
+++ bot.js
@@ -0,0 +1,126 @@
+#!/bin/env node
+var TelegramBot = require('node-telegram-bot-api');
+var request = require('request');
+var cheerio = require('cheerio');
+
+/* Bot setup */
+var token = 'YOUR_TOKEN'; //We read the token from a gitignored file
+
+// Setup polling way
+var bot = new TelegramBot(token);
+bot.setWebHook('YOUR_WEBHOOK' + bot.token);
+
+
+console.log('bot server started...');
+/* End bot setup */
+
+function isATitle(string) {
+    aux = false;
+    possibleTitles = ["Principal Translations", "Additional Translations",
+    "Compound Forms"];
+
+    for (var i in possibleTitles) {
+        if (string == possibleTitles[i]) 
+            aux = true;
+    }
+
+    return aux;
+}
+
+function translate(msg, destiny) {
+    //We need to send an user-agent for get wordreference's answer
+    var options = {
+      url: destiny,
+      headers: {
+        'User-Agent': 'request'
+      },
+      enconding: 'ascii'
+    };
+
+    function callback(error, response, body){
+        if(!error && response.statusCode==200){
+            var $ = cheerio.load(body);
+            var message='';
+            var thereIsTranslation=false;
+            (function(thereIsTranslation){
+                /*We need to extract all the rows*/
+                $(this).children('tr').each(function(){
+                    /*We want to separate the cells for output*/
+                    if ( ($(this).attr('class') != "langHeader") && ($(this).attr('class') != "odd") ) {
+                            console.log("There is translation!");
+                            thereIsTranslation = true;
+                            $(this).children('td').each(function() {
+                                
+                                //From the original we need to parse the word + next td (meaning)
+                                if ($(this).attr('class') == "FrWrd") {
+                                
+                                    cell =  $('<textarea />').html($(this).children('strong').html()).text();
+                                    message += '*' + cell + '* ';
+                                    //next td
+                                    cell =  $('<textarea />').html($(this).next('td').html()).text();
+                                    message += '_'+cell+'_' + '\n';
+                                }
+                                if ($(this).attr('class') == "ToWrd") {
+                                    //There is an em child on that cell that has additional info that we do not want to show
+                                    cell =  $('<textarea />').html($(this).clone()
+                                        .children() //select all the children
+                                        .remove()   //remove all the children
+                                        .end().html()).text();
+                                    message += cell;
+                                    message += '\n'; 
+                                   
+                                } else {
+                                    if (isATitle($(this).attr('title'))) {
+                                        message += '\n----------------------------------\n';
+                                        message +=  $(this).attr('title');
+                                        message += '\n----------------------------------\n';                                         
+                                    } 
+                                }
+                        });
+                    }            
+                });
+                if(!thereIsTranslation)
+                    message = '*There is no translation for ' + destiny.split('=')[1] + '*';
+            }).call($('table.WRD').first(),thereIsTranslation);
+            
+
+            //Message back
+            var fromId = msg.chat.id;
+            var options = {
+                parse_mode : 'Markdown'
+            };
+
+            bot.sendMessage(fromId, message, options);
+        }
+    }request(options, callback.bind(this)); //We need to inject msg
+}
+
+
+
+//Matches /eng [list,of,words]
+bot.onText(/\/eng (.+)/, function (msg, match) {
+  var wordsToSearch = match[1].split(','); //Array with words to traduce
+  for (word in wordsToSearch) {
+    translate(msg, 'http://www.wordreference.com/es/en/translation.asp?spen=' + wordsToSearch[word]);
+  }
+});
+
+//Matches /spa [lista,de,palabras]
+bot.onText(/\/spa (.+)/, function (msg, match) {
+  var wordsToSearch = match[1].split(','); //Array with words to traduce
+  for (word in wordsToSearch) {
+    translate(msg, 'http://www.wordreference.com/es/translation.asp?tranword=' + wordsToSearch[word]);
+  }
+  
+});
+//Matches /help
+bot.onText(/\/help/, function (msg, match) {
+  var fromId = msg.chat.id;
+  var resp = 'Modo de uso - Usage mode: \n' + 
+            '/eng lista,de,palabras : Traduce al Inglés la lista de palabras separadas por comas\n' +
+            '/spa list,of,words : Translate from English the list of words separated by commas\n' +
+            '/help display this message';
+  bot.sendMessage(fromId, resp);
+});
+
+module.exports = bot;
