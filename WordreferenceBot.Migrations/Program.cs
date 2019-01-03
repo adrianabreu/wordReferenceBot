24b9e6a574e83e287cd619c37f15b0f7020ca680
Adrian Abreu <aabreuglez@gmail.com>
2019-01-03 22:47:57 +0100

    Automatized migrations with dbup and basic schema added

---
+++ WordreferenceBot.Migrations/Program.cs
@@ -0,0 +1,37 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace WordreferenceBot.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Development.json", optional: true);
+
+            var configuration = builder.Build();
+
+            var db = configuration["SqlConnectionString"];
+
+            var upgradeEngine = DeployChanges.To
+                .SqlDatabase(db)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            Console.WriteLine($"Deploying changes to {db}");
+            upgradeEngine.PerformUpgrade();
+            #if DEBUG
+                Console.ReadKey();
+            #endif
+            Console.WriteLine("Finished migration");
+        }
+    }
+}
---
+++ WordreferenceBot.Migrations/Scripts/01-basic-tables.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[words] (
+	Id bigint NOT NULL,
+	Word nvarchar(200) NOT NULL,
+	PRIMARY KEY (Id)
+)
+
+CREATE TABLE [dbo].[translations] (
+	Id bigint NOT NULL,
+	Expression nvarchar(200) NOT NULL,
+	WordId bigint NOT NULL,
+	PRIMARY KEY (Id),
+	FOREIGN KEY (WordId) REFERENCES [dbo].[words](id) ON DELETE CASCADE
+)
+
+CREATE TABLE [dbo].[meanings] (
+	Id bigint NOT NULL,
+	[Value] nvarchar(200) NOT NULL,
+	TranslationId bigint NOT NULL,
+	PRIMARY KEY (Id),
+	FOREIGN KEY (Translationid) REFERENCES [dbo].[translations] ON DELETE CASCADE
+)
+
+CREATE TABLE [dbo].[possibletranslations] (
+	Id bigint NOT NULL,
+	[Value] nvarchar(200) NOT NULL,
+	TranslationId bigint NOT NULL,
+	PRIMARY KEY (Id),
+	FOREIGN KEY (Translationid) REFERENCES [dbo].[translations] ON DELETE CASCADE
+)
